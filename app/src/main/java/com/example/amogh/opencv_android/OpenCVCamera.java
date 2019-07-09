986298c8a5a9ce492bcc26ef64ee8de41f380718
Amogh <amogh112@gmail.com>
2019-07-09 19:39:35 +0530

    Visualisation of optical flow vectors from https://github.com/SureshLalchandani/Object-Motion-Detection-Optical-Flow/blob/master/MotionDetection/app/src/main/java/cs555/bucs/com/motiondetection/CameraActivity.java working

--- app/src/main/java/com/example/amogh/opencv_android/OpenCVCamera.java
+++ app/src/main/java/com/example/amogh/opencv_android/OpenCVCamera.java
@@ -6,64 +6,284 @@ import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
 
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+
+import java.util.List;
+
+
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.HOGDescriptor;
+import org.opencv.video.Video;
+
 public class OpenCVCamera extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
-
+    //
+//    private static final String TAG = "OpenCVCamera";
+//    private CameraBridgeViewBase cameraBridgeViewBase;
+//
+//    private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
+//        @Override
+//        public void onManagerConnected(int status) {
+//            switch (status) {
+//                case LoaderCallbackInterface.SUCCESS:
+//                    cameraBridgeViewBase.enableView();
+//                    break;
+//                default:
+//                    super.onManagerConnected(status);
+//                    break;
+//            }
+//        }
+//    };
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+//        setContentView(R.layout.activity_open_cvcamera);
+//        cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.camera_view);
+//        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+//        cameraBridgeViewBase.setCvCameraViewListener(this);
+//    }
+//
+//    @Override
+//    public void onResume(){
+//        super.onResume();
+//        if (!OpenCVLoader.initDebug()) {
+//            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+//            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, baseLoaderCallback);
+//        } else {
+//            Log.d(TAG, "OpenCV library found inside package. Using it!");
+//            baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+//        }
+//    }
+//    @Override
+//    public void onCameraViewStarted(int width, int height) {
+//
+//    }
+//
+//    @Override
+//    public void onCameraViewStopped() {
+//
+//    }
+//
+//    @Override
+//    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+//        return inputFrame.rgba();
+//    }
+//}
     private static final String TAG = "OpenCVCamera";
-    private CameraBridgeViewBase cameraBridgeViewBase;
 
-    private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                    cameraBridgeViewBase.enableView();
-                    break;
-                default:
-                    super.onManagerConnected(status);
-                    break;
-            }
-        }
-    };
+    private static final int VIEW_MODE_KLT_TRACKER = 0;
+    private static final int VIEW_MODE_OPTICAL_FLOW = 1;
+
+    private int mViewMode;
+    private Mat mRgba;
+    private Mat mIntermediateMat;
+    private Mat mGray;
+    private Mat mPrevGray;
+
+    MatOfPoint2f prevFeatures, nextFeatures;
+    MatOfPoint features;
+
+    MatOfByte status;
+    MatOfFloat err;
+
+    private MenuItem mItemPreviewOpticalFlow, mItemPreviewKLT;
+
+    private CameraBridgeViewBase mOpenCvCameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_open_cvcamera);
-        cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.camera_view);
-        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
-        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+
+        mViewMode = VIEW_MODE_OPTICAL_FLOW;
+//        mViewMode = VIEW_MODE_KLT_TRACKER;
     }
 
     @Override
-    public void onResume(){
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_camera, menu);
+//        mItemPreviewKLT = menu.add("KLT Tracker");
+//        mItemPreviewOpticalFlow = menu.add("Optical Flow");
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        if (item == mItemPreviewOpticalFlow) {
+//            mViewMode = VIEW_MODE_OPTICAL_FLOW;
+//            resetVars();
+//        } else if (item == mItemPreviewKLT) {
+//            mViewMode = VIEW_MODE_KLT_TRACKER;
+//            resetVars();
+//        }
+
+        return true;
+
+    }
+
+    private void resetVars() {
+        mPrevGray = new Mat(mGray.rows(), mGray.cols(), CvType.CV_8UC1);
+        features = new MatOfPoint();
+        prevFeatures = new MatOfPoint2f();
+        nextFeatures = new MatOfPoint2f();
+        status = new MatOfByte();
+        err = new MatOfFloat();
+    }
+
+
+    @Override
+    protected void onResume() {
         super.onResume();
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, baseLoaderCallback);
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
         } else {
             Log.d(TAG, "OpenCV library found inside package. Using it!");
-            baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV loaded successfully");
+
+                    mOpenCvCameraView.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
+    // Camera Code
+
     @Override
     public void onCameraViewStarted(int width, int height) {
-
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mIntermediateMat = new Mat(height, width, CvType.CV_8UC4);
+        mGray = new Mat(height, width, CvType.CV_8UC1);
+        resetVars();
     }
 
     @Override
     public void onCameraViewStopped() {
-
+        mRgba.release();
+        mGray.release();
+        mIntermediateMat.release();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return inputFrame.rgba();
+        mGray = inputFrame.gray();
+
+
+        final int viewMode = mViewMode;
+        switch (viewMode) {
+            case VIEW_MODE_OPTICAL_FLOW:
+
+                mGray = inputFrame.rgba();
+
+                if (features.toArray().length == 0) {
+                    int rowStep = 50, colStep = 100;
+                    int nRows = mGray.rows() / rowStep, nCols = mGray.cols() / colStep;
+
+                    Point points[] = new Point[nRows * nCols];
+                    for (int i = 0; i < nRows; i++) {
+                        for (int j = 0; j < nCols; j++) {
+                            points[i * nCols + j] = new Point(j * colStep, i * rowStep);
+                        }
+                    }
+
+                    features.fromArray(points);
+
+                    prevFeatures.fromList(features.toList());
+                    mPrevGray = mGray.clone();
+                    break;
+                }
+
+                nextFeatures.fromArray(prevFeatures.toArray());
+                Video.calcOpticalFlowPyrLK(mPrevGray, mGray,
+                        prevFeatures, nextFeatures, status, err);
+
+                List<Point> prevList = features.toList(), nextList = nextFeatures.toList();
+                Scalar color = new Scalar(255,0,0);
+
+                for (int i = 0; i < prevList.size(); i++) {
+                    Imgproc.line(mGray, prevList.get(i), nextList.get(i), color);
+                }
+
+                mPrevGray = mGray.clone();
+                break;
+            default:
+                mViewMode = VIEW_MODE_OPTICAL_FLOW;
+        }
+
+        return mGray;
     }
+
+
 }
